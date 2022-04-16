384bbabe4f6d2e24e20fe493c477a8ae246bf40f
Lex Miros <aleksei.miros@outlook.com>
2022-04-16 14:52:48 +1000

    Refactor

--- forms.py
+++ forms.py
@@ -5,12 +5,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class StaffAddFrom(FlaskForm):
-    staffID = IntegerField("Staff ID")
+    #staffID = IntegerField("Staff ID")
     firstName = StringField('FirstName', validators=[DataRequired(), Length(min = 2, max = 20)])
     lastName = StringField('LastName', validators=[DataRequired(), Length(min = 2, max = 20)])
     dateOfBirth = DateField("Date of birth", validators=[DataRequired()])
     startDate = DateField("Start Date", validators=[DataRequired()])
-    managerID = IntegerField("Manager ID if applicable")
+    managerID = StringField("Manager ID if applicable")
     farmLoc = StringField("Staff's primary farm", validators=[DataRequired()])
+    contactNumber = IntegerField("Primary Contact Number", validators=[DataRequired()])
     submit = SubmitField("Add Staff")
 
