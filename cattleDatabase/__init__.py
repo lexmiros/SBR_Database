e0d57d3f6f7d8cf694caa4e8241a3cc295a171d4
Lex Miros <aleksei.miros@outlook.com>
2022-05-23 17:14:30 +1000

    Remove global db connection

--- cattleDatabase/__init__.py
+++ cattleDatabase/__init__.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfjkhnfdgjijasdfjk' 
 
 #Database connection information
+
+
+#################################
+#Change to your information
 host_db='localhost' 
 user_db='adminflask'
 password_db='adminflask'
 database_db='project_db_4'
 charset_db='utf8mb4'
 cursorclass_db =pymysql.cursors.DictCursor
+#################################
 
-#conn = pymysql.connect(host='localhost', user='adminflask', password='adminflask', database='project_db_3', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
 
 
 #Turn the results from the database into a dictionary
